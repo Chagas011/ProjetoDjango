3b7990535dbea991f853fb5aac79a57f896d8299
Chagas011 <washington.chagas.9@hotmail.com>
2022-03-10 18:11:01 -0300

    models

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from recipes.views import home
-
+from django.urls import path, include
+from . import views
 urlpatterns = [
-    path('', home),
+    path('', views.home, name='recipes-home'),
+    path('recipes/<int:id>/', views.recipe, name='recipes-recipe'),
 ]
