d600119aa401528e028fc0d1c4626b6d249e3991
Chagas011 <washington.chagas.9@hotmail.com>
2022-03-18 18:26:10 -0300

    views test

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,7 @@
+from pickle import TRUE
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from . import views
 
 # Create your tests here.
 
@@ -12,3 +14,21 @@ class RecipeURLsTest(TestCase):
     def test_recipe_category_url(self):
         category_url = reverse('recipes:category', kwargs={'category_id': 1})
         self.assertEqual(category_url, '/recipes/category/1/')
+
+    def test_recipe_recipe_url(self):
+        recipe_url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(recipe_url, '/recipes/1/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_view(self):
+        viewc = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(viewc.func, views.category)
+
+    def test_recipe_recipe_view(self):
+        viewr = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(viewr.func, views.recipe)
