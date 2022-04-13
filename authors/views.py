7881234403ba02850ff42f69513cec2039f77fc5
Chagas011 <washington.chagas.9@hotmail.com>
2022-04-13 17:55:15 -0300

    criando formulario

--- authors/views.py
+++ authors/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from .forms import RegisterForm
+
 # Create your views here.
 
 
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    form = RegisterForm(request.POST)
+    return render(request, 'authors/pages/register_view.html', context={
+        'form': form,
+    })
